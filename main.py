8faf7771f113c532361e1ccc27c31135ef6084dd
alexw <alex.pepine2062@gmail.com>
2022-09-13 21:15:23 +0300

    asd

---
+++ main.py
@@ -0,0 +1,25 @@
+# print("Hello world")
+# print(1, 4.5, 3+5j)
+
+# type() - tipu de valoare
+# id() - adresa de locatie de memorie
+
+# print(type(1), type(4.5), type(3+5j))
+# print(id(1), id(4.5), id(3+5j))
+
+# int() - nr intregi
+# print(int(23), int(4.7))
+# float() - nr
+# print(float(2), float(4.7))
+# complex() - nr complexe
+# print(complex(3+5j), complex(3), complex(4.7))
+
+
+ # LISTE
+my_list=[1, 3.5, (3 + 2j), True, False, None, "ceva random", [1, 2, 3]]
+#        0   1       2       3     4     5        6            7
+#       -8  -7      -6      -5    -4    -3       -2           -1
+# print(my_list)
+# print(type(my_list))
+# print((my_list[2]))
+print(len(my_list))
