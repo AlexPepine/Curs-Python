3c410dbc05fac2c5adf04b66454e386e684ee81e
AlexPepine <alex.pepine2062@gmail.com>
2022-09-17 14:31:17 +0300

    1

---
+++ pythonProject/Tema_1.py
@@ -0,0 +1,14 @@
+# Tema Data Structures
+
+my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+print(my_list)
+my_list.sort()
+print(my_list)
+my_list.sort(reverse= True)
+print(my_list)
+my_list_slice = my_list[::-1]
+print(my_list_slice)
+my_list_slice = my_list[::1]
+print(my_list_slice)
+my_list_slice = my_list[::3]
+print(my_list_slice)
---
+++ pythonProject/Tema_2.py
@@ -0,0 +1,6 @@
+# Tema Conditional programming, loops, functions & modularityFile
+
+def your_function(1, 5, -3, 'abc', [12, 56, 'cad']):
+    return
+sum = your_function()
+print(sum)
