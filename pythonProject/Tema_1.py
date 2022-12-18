fc9792fb6f0459bb6954310a89ca27d8aafc3f20
alexw <alex.pepine2062@gmail.com>
2022-12-18 19:48:21 +0200

    asd

--- pythonProject/Tema_1.py
+++ pythonProject/Tema_1.py
@@ -1,14 +1,21 @@
-# Tema Data Structures
+# Tema 1 Data Structures
 
+# print the initial list
 my_list = [7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
 print(my_list)
+
+# sort from least to greatest
 my_list.sort()
 print(my_list)
+
+# sort from greatest to least
 my_list.sort(reverse= True)
 print(my_list)
+
 my_list_slice = my_list[::-1]
 print(my_list_slice)
 my_list_slice = my_list[::1]
 print(my_list_slice)
-my_list_slice = my_list[::3]
+#divizabile cu 3
+my_list_slice = my_list[1::3]
 print(my_list_slice)
--- pythonProject/Tema_2.py
+++ pythonProject/Tema_2.py
@@ -1,59 +1,50 @@
 # Tema Conditional programming, loops, functions & modularityFile
 
-def calcul(a, b, c, d, e):
-    return a + b + c
+def f1(*args, **kwargs):
+    s = 0
+
+    for param in args:
+        if type(param) == int or type(param) == float:
+            s += param
+
+    return s
+
+print(f1(1, 5, -3, 'abc', [12, 56, 'cad']))
+print(f1())
+print(f1(2, 4, 'abc', param_1=2))
 
 
-result = calcul(1, 5, -3, 'abc', [12, 56, 'cad'])
-print(result)
-
-
-def zero(a):
-    return a
-
-
-print(zero(0))
-
-
-def calcul_second(a, b, c, param_1=2):
-    return a+b
-
-
-result_second = calcul_second(2, 4, 'abc')
-print(result_second)
-
-
-def get_sum(n):
+# 2 #total, even, odd
+def f2(n):
     if n == 0:
-        return 0
-    return n + get_sum(n-1)
+        return 0, 0, 0
 
+    total, even, odd = f2(n-1)
+    total += n
 
-print(get_sum(10))
+    if n % 2 == 0:
+        even += n
+    else:
+        odd += n
 
+    return total, even, odd
 
-def get_sum_para(n):
-    # print(n)
-    if n == 2:
-        return 2
-    return n + get_sum_para(n-2)
-
-
-
-print(get_sum_para(10))
-
-
-def get_sum_impara(n):
-    # print(n)
-    if n == 1:
-        return 1
-    return n + get_sum_impara(n-2)
+n_total, n_even, n_odd = f2(5)
+print('total =', n_total)
+print('even = ', n_even)
+print('odd = ', n_odd)
 
 
 
-print(get_sum_impara(11))
+# Return the value inserted if the number is integer
+def f3():
+    x = input()
 
+    try:
+        x = int(x)
+    except ValueError:
+            x = 0
 
-val = input("Introdu numar")
+    return x
 
-print(int(val))
+print(f3())
