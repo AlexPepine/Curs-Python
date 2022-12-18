52f3ece23d10c65997f80fd08062033183258152
alexw <alex.pepine2062@gmail.com>
2022-12-18 20:21:49 +0200

    asd

---
+++ pythonProject/Tema_3/main.py
@@ -0,0 +1,8 @@
+from functions import *
+
+if __name__ == "__main__":
+    user_input = user_input_f()
+    print(user_input)
+
+    if user_input is not None:
+        number_data = get_output(user_input)
